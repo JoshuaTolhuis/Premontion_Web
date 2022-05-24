508a67a5179de04d1487112166c7eadf2131767d
Joshua Tolhuis <jotolhuis@bioinf.nl>
2022-05-24 09:08:27 +0200

    bluh

--- src/main/java/nl/bioinf/premonition/models/PremonitionForm.java
+++ src/main/java/nl/bioinf/premonition/models/PremonitionForm.java
@@ -1,11 +1,8 @@
 package nl.bioinf.premonition.models;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.util.Arrays;
 import java.util.Objects;
 
 /*
@@ -16,57 +13,29 @@ Author: Joshua Tolhuis
 public class PremonitionForm {
     private MultipartFile file;
     private MultipartFile refFile;
-    private String removeEdges;
-    private String includeNRCs;
+    private Boolean removeEdges;
+    private Boolean includeNRCs;
     private String limited;
     private String test;
 
-    public String getTest() {
-        return test;
-    }
-
-    public void setTest(String test) {
-        this.test = test;
-    }
-
     public MultipartFile getRefFile() {
         return refFile;
-    }
-
-    public void setRefFile(MultipartFile refFile) {
-        this.refFile = refFile;
     }
 
     public MultipartFile getFile() {
         return file;
     }
 
-    public void setFile(MultipartFile file) {
-        this.file = file;
-    }
-
-    public String getRemoveEdges() {
+    public Boolean getRemoveEdges() {
         return removeEdges;
     }
 
-    public void setRemoveEdges(String removeEdges) {
-        this.removeEdges = removeEdges;
-    }
-
-    public String getIncludeNRCs() {
+    public Boolean getIncludeNRCs() {
         return includeNRCs;
-    }
-
-    public void setIncludeNRCs(String includeNRCs) {
-        this.includeNRCs = includeNRCs;
     }
 
     public String getLimited() {
         return limited;
-    }
-
-    public void setLimited(String limited) {
-        this.limited = limited;
     }
 
     @Override
@@ -76,7 +45,7 @@ public class PremonitionForm {
         PremonitionForm that = (PremonitionForm) o;
         return Objects.equals(file, that.file) && Objects.equals(refFile, that.refFile) && Objects.equals(removeEdges, that.removeEdges) && Objects.equals(includeNRCs, that.includeNRCs) && Objects.equals(limited, that.limited) && Objects.equals(test, that.test);
     }
-
+    
     @Override
     public int hashCode() {
         return Objects.hash(file, refFile, removeEdges, includeNRCs, limited, test);
