88ee4dcb61882ccfbdb54c000f8aee13f64f8bbb
jotolhuis <j.o.tolhuis@st.hanze.nl>
2022-06-09 16:01:50 +0200

    updated error handling of file upload system

--- src/main/java/nl/bioinf/premonition/webcontrol/DownloadController.java
+++ src/main/java/nl/bioinf/premonition/webcontrol/DownloadController.java
@@ -1,4 +1,6 @@
 package nl.bioinf.premonition.webcontrol;
+import nl.bioinf.premonition.util.SessionUtil;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.InputStreamResource;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
@@ -6,15 +8,23 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import javax.servlet.http.HttpSession;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 
 @Controller
 public class DownloadController {
+
+    @Value("${data.out.location}")
+    String filelocation;
+
     @GetMapping("/download")
-    public ResponseEntity<InputStreamResource> download(String param) throws IOException {
-        File downloadableFile = new File("/homes/jotolhuis/Documents/Minor/tests/src/main/resources/downloads/testfile.txt");
+    public ResponseEntity<InputStreamResource> download(HttpSession session) throws IOException {
+        String userid = SessionUtil.getUserID(session);
+
+        // File downloadableFile = new File(filelocation+userid+".json");
+        File downloadableFile = new File(filelocation+"123456"+".json");
         HttpHeaders headers = new HttpHeaders();
         headers.add("Cache-Control", "no-cache, no-store, must-revalidate");
         headers.add("Pragma", "no-cache");
