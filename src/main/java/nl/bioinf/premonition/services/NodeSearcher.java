4faf52175771bb6a23310af882612950c6598992
jotolhuis <j.o.tolhuis@st.hanze.nl>
2022-06-14 15:50:59 +0200

    updated clickable function, it now remembers the canvas. search engine also updated to only show when canvas is present

--- src/main/java/nl/bioinf/premonition/services/NodeSearcher.java
+++
@@ -1,8 +0,0 @@
-package nl.bioinf.premonition.services;
-
-public class NodeSearcher {
-
-    // Open JSON output file from DataOut
-    // rewrite using given search file (DONT DELETE ORIGINAL)
-    //return new JSON with said node and connected nodes
-}
